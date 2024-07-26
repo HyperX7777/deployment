66d5aff1280f065d5bac72ddd9ddc9ca677b164d
Muhammad Ali <muhammadalijamal41@gmail.com>
2024-07-26 12:18:36 +0500

    deploy

--- app/main.py
+++ app/main.py
@@ -6,3 +6,7 @@ app = FastAPI()
 @app.get("/")
 def read_root():
     return {"Hello": "World1", "World2": "Pakistan zinda bad 1234567 80"}
+
+@app.get("/about")
+def index():
+    return {"Hello": "World1", "World2": "Pakistan zinda bad 1234567 80"}
